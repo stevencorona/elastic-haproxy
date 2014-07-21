37302d2131da779465735ca6a586810663130263
Steve Corona <stevecorona@me.com>
2014-07-21 05:58:32 -0700

    extract config into own file

---
+++ config.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"github.com/BurntSushi/toml"
+)
+
+type Config struct {
+	EnableStad       bool
+	EnableAutosclale bool
+	EnableRoute53    bool
+	HaproxySocket    string
+}
+
+func LoadConfig(path string) (config *Config) {
+	if _, err := toml.DecodeFile("config.toml", &config); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	return config
+}
--- elastic-haproxy.go
+++ elastic-haproxy.go
@@ -2,22 +2,11 @@ package main
 
 import (
 	"fmt"
-	"github.com/BurntSushi/toml"
 )
 
-type Config struct {
-	EnableStad       bool
-	EnableAutosclale bool
-	EnableRoute53    bool
-	HaproxySocket    string
-}
-
 func main() {
-	var conf Config
-	if _, err := toml.DecodeFile("config.toml", &conf); err != nil {
-		fmt.Println(err)
-		return
-	}
+
+	conf := LoadConfig("config.toml")
 
 	haproxy := new(Haproxy)
 	haproxy.Socket = conf.HaproxySocket
