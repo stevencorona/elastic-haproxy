a9374873f13d9e0c37602818c5e8df74032e9b9a
Steve Corona <stevecorona@me.com>
2014-07-22 05:48:11 -0700

    most basic api level support for CreateLoadBalancer

---
+++ api.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func SetupApiHandlers() {
+
+	http.HandleFunc("/", ELBHandler)
+	//http.HandleFunc()
+
+	http.ListenAndServe(":8080", nil)
+}
+
+func ELBHandler(w http.ResponseWriter, r *http.Request) {
+	action := r.FormValue("Action")
+	version := r.FormValue("Version")
+
+	fmt.Println(action, version)
+
+	// This is going to be nasty, but we need to redispatch the
+	// request to the correct handler.
+	switch action {
+	case "CreateLoadBalancer":
+		CreateLoadBalancerHandler(w, r)
+	}
+}
+
+func CreateLoadBalancerHandler(w http.ResponseWriter, r *http.Request) {
+	loadBalancerName := r.FormValue("LoadBalancerName")
+	fmt.Println(loadBalancerName)
+}
--- elastic-haproxy.go
+++ elastic-haproxy.go
@@ -21,4 +21,6 @@ func main() {
 
 	fmt.Println(serverInfo)
 
+	SetupApiHandlers()
+
 }
