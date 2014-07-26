6f9c8910dfb147bb88f1a9f891ebbc2583efd80b
Steve Corona <stevecorona@me.com>
2014-07-26 14:52:22 -0700

    move options to a seperate file

--- api.go
+++ api.go
@@ -5,50 +5,6 @@ import (
 	"net/http"
 )
 
-type CreateLoadBalancerOptions struct {
-	AvailabilityZones string
-	Listeners         []ListenerOptions
-	LoadBalancerName  string
-	Scheme            string
-	SecurityGroups    []string
-	Subnets           []string
-}
-
-type CreateLoadBalancerListenersOptions struct {
-	Listeners        []ListenerOptions
-	LoadBalancerName string
-}
-
-type CreateLoadBalancerPolicyOptions struct {
-	PolicyAttributes []PolicyAttributeOptions
-	PolicyName       string
-	PolicyTypeName   string
-}
-
-type RegisterInstancesWithLoadBalancerOptions struct {
-	Instances        []InstanceOptions
-	LoadBalancerName string
-}
-
-type ListenerOptions struct {
-	LoadBalancerPort string
-	Protocol         string
-	InstancePort     string
-	InstanceProtocol string
-	SSLCertificateId string
-	SSLCertificate   string
-}
-
-type InstanceOptions struct {
-	InstanceId string
-	IpAddress  string
-}
-
-type PolicyAttributeOptions struct {
-	AttributeName  string
-	AttributeValue string
-}
-
 func SetupApiHandlers() {
 	http.HandleFunc("/", ELBHandler)
 	http.ListenAndServe(":8080", nil)
