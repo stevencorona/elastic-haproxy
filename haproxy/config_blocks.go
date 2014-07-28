e31862c91f13fb3d3aa414f3390ef05284eb8f51
Steve Corona <stevecorona@me.com>
2014-07-28 05:41:41 -0700

    rename config

--- haproxy/config_blocks.go
+++ haproxy/config_blocks.go
@@ -1,21 +1,32 @@
 package haproxy
 
+type Config struct {
+	Global   GlobalBlock
+	Defaults DefaultBlock
+	//Frontends []FrontendBlock
+}
+
 type GlobalBlock struct {
-	Daemon          bool
-	Maxconn         int
+	Daemon          bool `config:"daemon %s"`
+	Maxconn         int  `config:"maxconn %s"`
 	CaBase          string
 	Chroot          string
 	CrtBase         string
 	Gid             int
 	Group           string
 	LogSendHostname string
+	LogTag string
+	CpuMap string
 	Nbproc          int
 	Pidfile         string
 	Uid             int
 	UlimitN         int
 	User            string
-	Stats           string
-	SslServerVerify bool
+	StatsBind       string
+	StatsSocket     string
+	SslDefaultBindCiphers string
+	SslDefaultServerCiphers string
+	SslServerVerify string
 	Node            string
 	Description     string
 	UnixBind        string
@@ -26,16 +37,16 @@ type GlobalBlock struct {
 		Facility string
 		MaxLevel string
 		MinLevel string
-	}
+	} `config:"log %s %s %s %s %s"`
 }
 
-type DefaultsBlock struct {
+type DefaultBlock struct {
 	Mode    string
-	Timeout TimeoutBlock
+	Timeout []TimeoutBlock
+	Options map[string]string
 }
 
 type TimeoutBlock struct {
-	Connect int
-	Client  int
-	Server  int
+	Type   string
+	Amount string
 }
